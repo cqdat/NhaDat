52cfa6ee15208e303c179bc2769fed13becde2b7
Dat Cao <quocdat_cao@yahoo.com>
2017-11-21 22:34:39 +0700

    MENUs

---
+++ bds/Areas/Cpanel/Controllers/MENUController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bds.Areas.Cpanel.Models;
+
+namespace bds.Areas.Cpanel.Controllers
+{
+    public class MENUController : Controller
+    {
+        private DB_BDSEntitiesAdmin db = new DB_BDSEntitiesAdmin();
+
+        // GET: Cpanel/MENU
+        public ActionResult Index()
+        {
+            var model = db.MENUs.Where(q => q.IdCha == 0).OrderBy(o => o.ThuTu);
+
+            return View(model);
+        }
+
+        // GET: Cpanel/MENU/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MENU mENU = db.MENUs.Find(id);
+            if (mENU == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mENU);
+        }
+
+        // GET: Cpanel/MENU/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCha = new SelectList(db.MENUs.Where(l => l.IdCha == 0), "IdMenu", "TenMenu");
+            return View();
+        }
+
+        // POST: Cpanel/MENU/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdMenu,TenMenu,IdCha,ThuTu,HienThi,IsMenu,url")] MENU mENU)
+        {
+            if (ModelState.IsValid)
+            {
+                if(mENU.IdCha == null)
+                {
+                    mENU.IdCha = 0;
+                }
+                db.MENUs.Add(mENU);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mENU);
+        }
+
+        // GET: Cpanel/MENU/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MENU mENU = db.MENUs.Find(id);
+            if (mENU == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mENU);
+        }
+
+        // POST: Cpanel/MENU/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdMenu,TenMenu,IdCha,ThuTu,HienThi,IsMenu,url")] MENU mENU)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mENU).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mENU);
+        }
+
+        // GET: Cpanel/MENU/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MENU mENU = db.MENUs.Find(id);
+            if (mENU == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mENU);
+        }
+
+        // POST: Cpanel/MENU/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MENU mENU = db.MENUs.Find(id);
+            db.MENUs.Remove(mENU);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
