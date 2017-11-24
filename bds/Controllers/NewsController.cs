2798a619e1aa6b209e9cfb4568c661b146bd1a8e
tuanb <tuanb@OANH>
2017-11-24 22:56:14 +0700

    24112017

--- bds/Controllers/NewsController.cs
+++ bds/Controllers/NewsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using bds.Models;
+using bds.Areas.Cpanel.Models;
 
 namespace bds.Controllers
 {
     public class NewsController : Controller
     {
+        DB_BDSEntitiesAdmin db = new DB_BDSEntitiesAdmin();
         // GET: News
         public ActionResult Index()
         {
@@ -16,12 +19,21 @@ namespace bds.Controllers
 
         public ActionResult Thread(int? id)
         {
-            return View();
+            NewsViewModel model = new NewsViewModel();
+            model.TinTuc = db.BDS_TinTuc.Where(q => q.IDMenu == id && q.Visible == true).Take(15).OrderByDescending(o => o.CreateBy).ToList();
+            model.TinNoiBat = db.BDS_TinTuc.Where(q => q.NoiBat == true && q.Visible == true).Take(15).OrderByDescending(o => o.CreateBy).ToList();
+            model.NhieuNguoiDoc = db.BDS_TinTuc.Where(q => q.NhieuNguoiDoc == true && q.Visible == true).Take(15).OrderByDescending(o => o.CreateBy).ToList();
+            return View(model);
         }
 
         public ActionResult Detail(int? id)
         {
-            return View();
+            NewsDetailModel model = new NewsDetailModel();
+            model.ChiTiet = db.BDS_TinTuc.Find(id);
+            model.TinKhac = db.BDS_TinTuc.Where(q => q.Visible == true).ToList();
+            model.TinNoiBat = db.BDS_TinTuc.Where(q => q.NoiBat == true && q.Visible == true).Take(15).OrderByDescending(o => o.CreateBy).ToList();
+            model.NhieuNguoiDoc = db.BDS_TinTuc.Where(q => q.NhieuNguoiDoc == true && q.Visible == true).Take(15).OrderByDescending(o => o.CreateBy).ToList();
+            return View(model);
         }
     }
 }
---
+++ bds/Models/NewsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using bds.Areas.Cpanel.Models;
+
+namespace bds.Models
+{
+    public class NewsViewModel
+    {
+        public List<BDS_TinTuc> TinTuc;
+        public List<BDS_TinTuc> TinNoiBat;
+        public List<BDS_TinTuc> NhieuNguoiDoc;
+    }
+}
