387b36028e0e60e0aa55c48c6d87fe96d9ee41b0
tuanb <tuanb@OANH>
2018-01-09 20:40:14 +0700

    t

--- bds/Areas/Cpanel/Models/DB_BDSEntitiesAdmin.cs
+++ bds/Areas/Cpanel/Models/DB_BDSEntitiesAdmin.cs
@@ -27,6 +27,7 @@ namespace bds.Areas.Cpanel.Models
         public virtual DbSet<GIOITHIEU> GIOITHIEUx { get; set; }
         public virtual DbSet<INFORMATION> INFORMATION { get; set; }
         public virtual DbSet<MENU> MENUs { get; set; }
+        public virtual DbSet<TABDUAN> TABDUANs { get; set; }
         public virtual DbSet<THANHVIEN> THANHVIENs { get; set; }
         public virtual DbSet<THUOCTINH> THUOCTINHs { get; set; }
         public virtual DbSet<TINHTHANH> TINHTHANHs { get; set; }
